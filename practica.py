fda20464eb246a1aee6f80b2e143385d63cfc8d0
MateoFlorez777 <juanmateopro77@gmail.com>
2025-03-27 17:01:25 -0500

    se actualiza la limpieza

--- practica.py
+++ practica.py
@@ -22,6 +22,9 @@ DM_ESTADO.columns=DM_ESTADO.columns.str.replace('ESTADO','NOMBRE')
 DM_ESTADO["IDESTADO"]=range(1,len(DM_ESTADO)+1)
 data = pd.merge(DM_ESTADO,data,left_on="NOMBRE",right_on="ESTADO",how="right")
 
+DM_ESTADO=DM_ESTADO[['IDESTADO','NOMBRE']]
+
+#eliminar del dataset la columna ESTADO y NOMBRE
 
 del data['ESTADO']
 del data['NOMBRE']
@@ -39,6 +42,11 @@ DM_ATENCION["IDATENCION"]=range(1,len(DM_ATENCION)+1)
 DM_ATENCION.columns=DM_ATENCION.columns.str.replace('ATENCION','NOMBRE')
 DM_ATENCION.columns=DM_ATENCION.columns.str.replace('IDNOMBRE','IDATENCION')
 data = pd.merge(DM_ATENCION,data,left_on="NOMBRE",right_on="ATENCION",how="right")
+
+DM_ATENCION=DM_ATENCION[['IDATENCION','NOMBRE']]
+
+#eliminar del dataset la columna ATENCION y NOMBRE
+
 del data['ATENCION']
 del data['NOMBRE']
 
@@ -67,6 +75,10 @@ DM_PAIS.columns=DM_PAIS.columns.str.replace('IDNOMBRE','IDPAIS')
 
 data = pd.merge(DM_PAIS,data,left_on="NOMBRE",right_on="PAIS",how="right")
 
+DM_PAIS=DM_PAIS[['IDPAIS', 'NOMBRE']]
+
+#eliminar del dataset la columna NOMBRE Y PAIS
+
 del data['NOMBRE']
 del data['PAIS']
 
@@ -89,6 +101,9 @@ DM_DEPARTAMENTO['IDDPTO']=DM_DEPARTAMENTO['IDCIUDAD']//1000
 DM_DEPARTAMENTO.drop_duplicates(inplace=True)
 del DM_DEPARTAMENTO['IDCIUDAD']
 data['IDDPTO']=data['IDCIUDAD']//1000
+
+#eliminar del dataset la columna DEPARTAMENTO
+
 del data['DEPARTAMENTO']
 DM_DEPARTAMENTO.columns=DM_DEPARTAMENTO.columns.str.replace('DEPARTAMENTO','NOMBRE')
 DM_DEPARTAMENTO=DM_DEPARTAMENTO[['IDDPTO','NOMBRE']]
@@ -106,6 +121,11 @@ DM_TIPO.columns=DM_TIPO.columns.str.replace('TIPO','NOMBRE')
 
 DM_TIPO['IDTIPO']=range(1,len(DM_TIPO)+1)
 data=pd.merge(DM_TIPO,data,left_on='NOMBRE',right_on='TIPO', how="right")
+
+DM_TIPO=DM_TIPO[['IDTIPO','NOMBRE']]
+
+#eliminar del dataset la columna NOMBRE Y TIPO
+
 del data['NOMBRE']
 del data['TIPO']
 
@@ -122,6 +142,14 @@ DM_FECHA.columns=DM_FECHA.columns.str.replace('FECHA','NOMBRE')
 
 DM_FECHA['IDFECHA']=range(1,len(DM_FECHA)+1)
 DM_FECHA=DM_FECHA[['IDFECHA','NOMBRE', 'ANUAL', 'MES', 'DIA']]
+
+# CATEGORIZAR UNA COLUMNA
+
+rangos= [0,5,10,17,59,100,110]
+nombre=['BEBE','NIÑO','ADOLESCENTE','ADULTO','ADULTO MAYOR','ADULTO MUY MAYOR']
+data['GEDAD']=pd.cut(data['EDAD'],rangos,labels=nombre)
+del data['FECHA']
+data1=data[['ID','IDPAIS','IDESTADO','IDTIPO','IDATENCION','IDDPTO','IDCIUDAD','EDAD','SEXO']]
 
 
 #GENERAR ARCHIVOS .CSV
